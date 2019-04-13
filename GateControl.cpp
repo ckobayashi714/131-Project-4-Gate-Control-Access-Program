2fe9c51b02f8cd320644ea77223f8c9bf3ebb53e
ckobayashi714 <ckobayashi@csu.fullerton.edu>
2019-04-13 08:49:31 -0700

    some tests pass

--- GateControl.cpp
+++ GateControl.cpp
@@ -38,11 +38,16 @@ bool	GateControl::AccessAllowed(CardNumber number)
 //	GateControl::AddAuthorization
 //****************************************************************************************
 bool	GateControl::AddAuthorization(CardNumber number, const string& name,
-									  const string& startTime, const string& endTime)
-{
+									  const string& startTime, const string& endTime) {
 	Authorization a1(number, name, startTime, endTime);
-
-	return true;
+	AuthorizationIterator temp;
+	temp = authorizationMap_.find(a1.number_);
+	if (temp == authorizationMap_.end()) {
+		authorizationMap_.emplace(number, a1);
+		return true;
+	}
+	else
+		return false;
 }
 //****************************************************************************************
 //	GateControl::ChangeAuthorization
@@ -50,43 +55,62 @@ bool	GateControl::AddAuthorization(CardNumber number, const string& name,
 bool	GateControl::ChangeAuthorization(CardNumber number, const string& name,
 										 const string& startTime, const string& endTime)
 {
-	return false;
+	AuthorizationIterator temp;
+	temp = authorizationMap_.find(number);
+	if (temp == authorizationMap_.end())
+		return false;
+  else
+		return true;
 }
 //****************************************************************************************
 //	GateControl::DeleteAuthorization
 //****************************************************************************************
-bool	GateControl::DeleteAuthorization(CardNumber number)
-{
-	return false;
+bool	GateControl::DeleteAuthorization(CardNumber number) {
+	AuthorizationIterator temp;
+	temp = authorizationMap_.find(number);
+	if (temp == authorizationMap_.end()) {
+		return false;
+	}
+	else {
+		authorizationMap_.erase(temp);
+		return true;
+	}
 }
 //****************************************************************************************
 //	GateControl::GetAllAuthorizations
 //****************************************************************************************
-void	GateControl::GetAllAuthorizations(AuthorizationVector& authorizationVector)
-{
+void	GateControl::GetAllAuthorizations(AuthorizationVector& authorizationVector) {
 	AuthorizationIterator	iterator;
-	//iterator.begin() = authorizationVector;
-	//return;
+	iterator = authorizationMap_.begin();
+	authorizationVector.clear();
+	if (authorizationMap_.empty())
+		return;
+	for (; iterator != authorizationMap_.end(); ++iterator){
+		authorizationVector.push_back(iterator->second);
+	}
 }
 //****************************************************************************************
 //	GateControl::GetAllTransactions
 //****************************************************************************************
-void	GateControl::GetAllTransactions(TransactionVector& transactionVector)
-{
+void	GateControl::GetAllTransactions(TransactionVector& transactionVector) {
 	//return false;
 }
 //****************************************************************************************
 //	GateControl::GetCardAuthorization
 //****************************************************************************************
-bool	GateControl::GetCardAuthorization(CardNumber number, Authorization& authorization)
-{
-	return false;
+bool	GateControl::GetCardAuthorization(CardNumber number, Authorization& authorization) {
+		AuthorizationIterator temp;
+		temp = authorizationMap_.find(number);
+		if (temp == authorizationMap_.end())
+			return false;
+		else
+			authorization = authorizationMap_.at(number);
+			return true;
 }
 //****************************************************************************************
 //	GateControl::GetCardTransactions
 //****************************************************************************************
 bool	GateControl::GetCardTransactions(CardNumber number,
-										 TransactionVector& transactionVector)
-{
+										 TransactionVector& transactionVector) {
 	return false;
 }
